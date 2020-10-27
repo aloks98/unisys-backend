79f0dd279b93a407cc54e14129e2df93d47330c8
juanvera11127 <35055901+juanvera11127@users.noreply.github.com>
2020-10-27 16:36:37 -0700

    Create assignmentModel.js

---
+++ models/assignmentModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const assignmentSchema = mongoose.Schema({
+  assignmentId: {
+    type: int,
+    required: true,
+  },
+  assignmentName: {
+    type: String,
+    required: true,
+  },
+  grade: {
+    type: String,
+    required: true,
+  },
+  dateDue: {
+    type: Date,
+    required: true,
+  },
+  course: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Course',
+  },
+  student: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Student',
+  },
+});
